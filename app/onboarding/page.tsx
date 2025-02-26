7ce7f1b630aecb7b27f3dd7ac8ce8de45a08ac35
rmourey26 <rmourey_jr@quantumone.io>
2025-02-26 17:00:53 -0500

    .

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -44,10 +44,10 @@ export default function OnboardingPage() {
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white md:flex dark:border-r">
           <div className="absolute inset-0 bg-gradient-to-r from-gray-700 via-gray-900 to-black">
           <Image
-          src="https://quantumone.b-cdn.net/onyx/resendit-ob2.jpg"
-          width={2048}
-          height={2048}b
-          alt="Onboarding-two"
+          src="https://quantumone.b-cdn.net/onyx/resendit-ob3.jpg"
+          width={576}
+          height={448}
+          alt="Onboarding-three"
           style={{objectFit: "contain"}}
           
         />
