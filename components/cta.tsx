e85b80eae459524e1da5e036623f2c51f05069a4
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-27 20:26:57 -0500

    Update cta.tsx

--- components/cta.tsx
+++ components/cta.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Link from 'next/link'
 import { motion } from "framer-motion"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
