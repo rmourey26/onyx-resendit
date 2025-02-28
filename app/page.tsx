7c9224fb5808d3b14b08e77cc5c2ab884c7b8c8c
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-28 15:50:10 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default async function IndexPage() {
 
            <div className="place-self-center max-w-5xl aspect-video aspect-w-16 aspect-h-9 overflow-hidden rounded-lg shadow-lg space-y-8">
 
-<iframe width="100% height="100%" src="https://www.youtube.com/embed/rq0lS7ZKYFQ?si=GTgF6H3Rr78LRA2z" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+<iframe width="100%" height="100%" src="https://www.youtube.com/embed/rq0lS7ZKYFQ?si=GTgF6H3Rr78LRA2z" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 </div>
         <p className="mt-4 max-w-2xl text-lg text-center text-muted-foreground sm:text-xl">
          Sustainable, brandable, reusable with blockchain and AI technology under the hood.  
