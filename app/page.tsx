3343edda20f95c053fbd87bfd6b7bf2ca0240fad
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-27 22:57:23 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default async function IndexPage() {
 		return redirect("/dashboard");
 	}
   return (
-            <section className="container max-w-7xl mx-auto flex flex-1 flex-col space-y-8 items-center gap-6 pb-24 pt-10 sm:pt-32 sm:pb-24">
+            <section className="container max-w-7xl mx-auto flex flex-1 flex-col space-y-8 items-center gap-6 pb-16 pt-8 sm:pt-16 sm:pb-24">
       <div className="flex mx-auto flex-col px-4 md:px-6 lg:px-8 items-center text-center gap-2">
         <h1 className="text-4xl sm:text-5xl lg:text-7xl font-extrabold leading-tight tracking-tighter md:text-6xl mb-4">
           Packagings Paradigm Shift 
