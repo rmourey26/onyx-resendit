790db377890d7654acd9234f4c4b6fb5cd02ba20
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-27 19:07:13 -0500

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -29,7 +29,7 @@ export default async function ContactPage() {
                 Contact Us
               </h1>
               <p className="text-sm text-muted-foreground">
-                Rather talk? Call us at +1-555-867-5309.
+                Request a meeting or send us an inquiry.
               </p>
             </div>
                         <Contact/>
