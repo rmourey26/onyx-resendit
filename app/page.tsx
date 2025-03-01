d9069efef4d0fad5fe5851fbf45ea28634009f70
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-03-01 14:38:28 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,9 +29,9 @@ export default async function IndexPage() {
           Sustainable Shipping Reimagined
         </h1>
 
-           <div className="container items-center flex"> 
+           <div className="container flex"> 
 
-<iframe className="aspect-video object-fill overflow-hidden rounded-lg shadow-lg space-y-8 lg:min-h-500" src="https://www.youtube.com/embed/rq0lS7ZKYFQ?si=GTgF6H3Rr78LRA2z"></iframe>
+<iframe className="aspect-video place-self-center overflow-hidden rounded-lg shadow-lg space-y-8 lg:min-h-500" src="https://www.youtube.com/embed/rq0lS7ZKYFQ?si=GTgF6H3Rr78LRA2z"></iframe>
 </div>
         <p className="mt-2 max-w-2xl text-lg text-center text-muted-foreground sm:text-xl">
 Reduce waste, cut costs, and enhance logistics with our innovative, seamless solution. Smart, durable, eco-friendly, powered by blockchain tracking, rewards, and AI insight, Resend-It fuels the circular economy. 
