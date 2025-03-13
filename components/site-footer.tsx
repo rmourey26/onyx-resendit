44a17882ed6459389a2ab1a0497ddb8f61ca2ef1
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-03-13 19:31:30 -0400

    Update site-footer.tsx

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -85,7 +85,7 @@ export function SiteFooter() {
                   variant: "ghost",
                 })}
               >
-                <Icons.twitter className="h-5 w-5" />
+                <Icons.linkedin className="h-5 w-5" />
                 <span className="sr-only">LinkedIn</span>
               </div>
             </Link>
