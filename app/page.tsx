456f0df575595e61ef943ef832a40a0ac0606c42
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-04-22 16:11:45 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,16 +24,10 @@ export default async function IndexPage() {
 	}
   return (
             <section className="relative max-w-dvw w-full overflow-hidden">
-
- <div className="container px-4 md:px-6 mx-auto flex flex-col space-y-16 items-center gap-y-10 pb-16 pt-16 sm:pt-16 sm:pb-24">
+<div className="container px-4 md:px-6 mx-auto flex flex-col space-y-16 items-center gap-y-10 pb-16 pt-16 sm:pt-16 sm:pb-24">
       <div className="flex mx-auto flex-col px-4 md:px-6 lg:px-8 w-full items-center gap-y-16">
-        <h1 className="text-4xl sm:text-5xl lg:text-7xl font-extrabold leading-tight tracking-tighter md:text-6xl xs:text-left md: text-center">
-          Sustainable Shipping Reimagined
-        </h1>
-
-  
-
-<iframe className="aspect-video max-w-3xl w-full place-self-center overflow-hidden rounded-lg shadow-lg" src="https://www.youtube.com/embed/rq0lS7ZKYFQ?si=GTgF6H3Rr78LRA2z"></iframe>
+        <h1 className="text-4xl sm:text-5xl lg:text-7xl font-extrabold leading-tight tracking-tighter md:text-6xl md:text-center">Sustainable Shipping Reimagined</h1>
+ <iframe className="aspect-video max-w-3xl w-full place-self-center overflow-hidden rounded-lg shadow-lg" src="https://www.youtube.com/embed/rq0lS7ZKYFQ?si=GTgF6H3Rr78LRA2z"></iframe>
         <p className="max-w-3xl text-xl xs:text-justify text-muted-foreground">Reduce waste, reinvent optimization, and maximize revenue while rewarding your customers like never before with our Smart Packaging as-a Service platform.</p>
        </div>
        </div>
