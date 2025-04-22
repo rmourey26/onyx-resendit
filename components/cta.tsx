fab753dd22edd7da23f7f4d5f5aade873a997634
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-04-22 15:18:49 -0400

    Update cta.tsx

--- components/cta.tsx
+++ components/cta.tsx
@@ -51,7 +51,7 @@ export default function Cta() {
           target="_blank"
           rel="noreferrer"
           href={siteConfig.links.contact}
-          className={buttonVariants({ variant: "outline"})}
+          className={buttonVariants({ variant: "primary"})}
         >
           Get Started
         </Link>
